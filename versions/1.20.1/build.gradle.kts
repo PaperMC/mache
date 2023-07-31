cadb8f1e5f671b1b6752ab310da7f6ac0fc69f0e
Kyle Wood <kyle@denwav.dev>
2023-07-30 21:08:09 -0500

    Bump Parchment

--- versions/1.20.1/build.gradle.kts
+++ versions/1.20.1/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     codebook("1.0.5")
     remapper(art("1.0.5"))
     decompiler(vineflower("1.9.1"))
-    parchment("1.20.1", "2023.07.23")
+    parchment("1.20.1", "2023.07.30")
 }
 
 dependencies {
