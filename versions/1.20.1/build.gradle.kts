92ed5f4811da20aa69230c4ff0321c01d76622c2
Kyle Wood <kyle@denwav.dev>
2023-08-05 17:22:46 -0500

    Bump some dependency versions, output doesn't change

--- versions/1.20.1/build.gradle.kts
+++ versions/1.20.1/build.gradle.kts
@@ -7,9 +7,9 @@ mache {
 }
 
 dependencies {
-    codebook("1.0.5")
+    codebook("1.0.6")
     remapper(art("1.0.5"))
-    decompiler(vineflower("1.9.1"))
+    decompiler(vineflower("1.9.2"))
     parchment("1.20.1", "2023.07.30")
 }
 
