b8291ae8450fb4c7b7c907dadf669dc40fa15b54
MiniDigger | Martin <admin@benndorf.dev>
2023-09-09 11:59:21 +0200

    try out vf10: fix patches for vf 1.10.0-snapshot

--- versions/1.20.2-pre2_vf10/build.gradle.kts
+++ versions/1.20.2-pre2_vf10/build.gradle.kts
@@ -6,10 +6,14 @@ mache {
     minecraftVersion = "1.20.2-pre2"
 }
 
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+}
+
 dependencies {
     codebook("1.0.6")
     remapper(art("1.0.7"))
-    decompiler(vineflower("1.9.3"))
+    decompiler(vineflower("1.10.0-SNAPSHOT"))
     parchment("1.20.1", "2023.09.03")
 }
 
