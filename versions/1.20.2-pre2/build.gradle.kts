afb69f0061944ab05f6c5354dabbb2ced216ba9b
MiniDigger | Martin <admin@benndorf.dev>
2023-09-09 10:28:15 +0200

    Fix patches for 1.20.2-pre2

--- versions/1.20.2-pre2/build.gradle.kts
+++ versions/1.20.2-pre2/build.gradle.kts
@@ -9,8 +9,8 @@ mache {
 dependencies {
     codebook("1.0.6")
     remapper(art("1.0.7"))
-    decompiler(vineflower("1.9.2"))
-    parchment("1.20.1", "2023.08.20")
+    decompiler(vineflower("1.9.3"))
+    parchment("1.20.1", "2023.09.03")
 }
 
 dependencies {
