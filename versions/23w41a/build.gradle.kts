b8a84d82196fed4b7f7fd611bbf1563124d12d63
Nassim Jahnke <nassim@njahnke.dev>
2023-10-15 10:05:57 +1000

    Prepare for 23w41a

--- versions/23w41a/build.gradle.kts
+++ versions/23w41a/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 mache {
-    minecraftVersion = "23w40a"
+    minecraftVersion = "23w41a"
     repositories.register("sonatype snapshots") {
         url.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         includeGroups.set(listOf("org.vineflower"))
