20ce8fd1252fbdc6d8eab2d32d1c4138865a8527
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-11-16 21:10:31 -0800

    23w46a

---
+++ versions/23w46a/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("mache")
+}
+
+mache {
+    minecraftVersion = "23w46a"
+    repositories.register("sonatype snapshots") {
+        url.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        includeGroups.set(listOf("org.vineflower"))
+    }
+}
+
+dependencies {
+    codebook("1.0.7")
+    remapper(art("1.0.7"))
+    decompiler(vineflower("1.10.0-SNAPSHOT"))
+    parchment("1.20.1", "2023.09.03")
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+}
