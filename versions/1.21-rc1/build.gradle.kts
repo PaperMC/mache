f3d722c0c383e8b2b8ef3e2a85d6b5b582d900e1
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2024-06-10 13:26:09 +0000

    Update to 1.21-rc1

--- versions/1.21-rc1/build.gradle.kts
+++ versions/1.21-rc1/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "1.21-pre4"
+    minecraftVersion = "1.21-rc1"
 
     repositories.register("sonatype snapshots") {
         url = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
