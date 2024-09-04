6ed02c463f05bce26dbf0304552df485c5402066
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2024-09-04 15:11:47 +0000

    Update to 24w36a

--- versions/24w36a/build.gradle.kts
+++ versions/24w36a/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "24w35a"
+    minecraftVersion = "24w36a"
 
     repositories.register("sonatype snapshots") {
         url = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
