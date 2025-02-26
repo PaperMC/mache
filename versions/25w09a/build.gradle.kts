b3f3a8aaaad8857c481d5a7aba89f5b629c1fcbc
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2025-02-26 16:01:54 +0000

    Update to 25w09a

--- versions/25w09a/build.gradle.kts
+++ versions/25w09a/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "25w08a"
+    minecraftVersion = "25w09a"
     minecraftJarType = MinecraftJarType.SERVER
 
     repositories.register("sonatype snapshots") {
