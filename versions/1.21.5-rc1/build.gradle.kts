e848e33853159602b4d22026a528cde8e8e639e5
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2025-03-20 16:35:55 +0000

    Update to 1.21.5-rc1

--- versions/1.21.5-rc1/build.gradle.kts
+++ versions/1.21.5-rc1/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "1.21.5-pre3"
+    minecraftVersion = "1.21.5-rc1"
     minecraftJarType = MinecraftJarType.SERVER
 
     repositories.register("sonatype snapshots") {
