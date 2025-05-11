cfd510ca348f0ed96aeced4bfb8d57c0851e8f45
Bjarne Koll <git@lynxplay.dev>
2025-05-11 22:18:12 +0200

    Bump codebook to 1.0.14

--- versions/1.21.6-pre1/build.gradle.kts
+++ versions/1.21.6-pre1/build.gradle.kts
@@ -31,7 +31,7 @@ mache {
 }
 
 dependencies {
-    codebook("1.0.13")
+    codebook("1.0.14")
     remapper(art("2.0.5"))
     decompiler(vineflower("1.11.1"))
     paramMappings("io.papermc.parchment.data:parchment-25w21a:2025.05.28")
