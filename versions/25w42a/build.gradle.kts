b6dbef58acf9ef9c99a5cd1ab68e372e17cd5a2c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-14 15:33:56 -0700

    Update parchment

--- versions/25w42a/build.gradle.kts
+++ versions/25w42a/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     codebook("1.0.15")
     remapper(art("2.0.5"))
     decompiler(vineflower("1.11.1"))
-    paramMappings("io.papermc.parchment.data:parchment:1.21.10+build.7")
+    paramMappings("io.papermc.parchment.data:parchment:25w42a+build.1")
 }
 
 dependencies {
