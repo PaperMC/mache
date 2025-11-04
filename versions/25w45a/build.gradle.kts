16f81730abe4606a1df2c5305c7178b51bb0ca48
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-11-04 10:34:08 -0700

    Update sculptor

--- versions/25w45a/build.gradle.kts
+++ versions/25w45a/build.gradle.kts
@@ -1,7 +1,7 @@
 import io.papermc.sculptor.shared.util.MinecraftJarType
 
 plugins {
-    id("io.papermc.sculptor.version") version "1.0.14"
+    id("io.papermc.sculptor.version") version "1.0.15"
 }
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
