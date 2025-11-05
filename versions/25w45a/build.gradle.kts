0d9584deabfbdf176f742ad93b287ecffe8c2470
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-11-04 17:37:48 -0700

    Update parchment

--- versions/25w45a/build.gradle.kts
+++ versions/25w45a/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     codebook("1.0.18")
     remapper(art("2.0.5"))
     decompiler(vineflower("1.11.2-SNAPSHOT"))
-    val parchment = "io.papermc.parchment.data:parchment:25w44a+build.2"
+    val parchment = "io.papermc.parchment.data:parchment:25w45a+build.1"
     paramMappings(parchment)
     constants(parchment)
 }
