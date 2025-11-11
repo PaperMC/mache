ccee85996824bf3f41ece833cc7704fce5090948
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2025-11-11 15:31:54 +0000

    Update to 25w46a

--- versions/25w46a/build.gradle.kts
+++ versions/25w46a/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "25w45a"
+    minecraftVersion = "25w46a"
     minecraftJarType = MinecraftJarType.SERVER
 
     val args = mutableListOf(
