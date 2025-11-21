4f3bd80c83a6c8c0718a3436ab3f129b67b70c92
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2025-11-21 14:29:30 +0000

    Update to 1.21.11-pre2

--- versions/1.21.11-pre2/build.gradle.kts
+++ versions/1.21.11-pre2/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "1.21.11-pre1"
+    minecraftVersion = "1.21.11-pre2"
     minecraftJarType = MinecraftJarType.SERVER
 
     val args = mutableListOf(
