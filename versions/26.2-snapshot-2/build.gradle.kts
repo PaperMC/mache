e4a958066e062d36dc4ed1271d52d3173fa5871c
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2026-04-15 19:51:11 +0200

    update unpick definitions

--- versions/26.2-snapshot-2/build.gradle.kts
+++ versions/26.2-snapshot-2/build.gradle.kts
@@ -34,7 +34,7 @@ tasks.withType<JavaCompile>().configureEach {
 dependencies {
     codebook("2.0.0-SNAPSHOT")
     decompiler(vineflower("1.11.2"))
-    constants("io.papermc.unpick-definitions:unpick-definitions:26.1+build.2")
+    constants("io.papermc.unpick-definitions:unpick-definitions:26.2-snapshot-2+build.1")
 }
 
 dependencies {
