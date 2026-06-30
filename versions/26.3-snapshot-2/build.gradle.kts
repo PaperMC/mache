ad27c63e160d2b9f9706aee76b8cacff7f46d355
Sculptor <166456271+mache-sculptor[bot]@users.noreply.github.com>
2026-06-30 15:38:51 +0000

    Update to 26.3-snapshot-2

--- versions/26.3-snapshot-2/build.gradle.kts
+++ versions/26.3-snapshot-2/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val generateReportsProperty = providers.gradleProperty("generateReports")
 mache {
-    minecraftVersion = "26.3-snapshot-1"
+    minecraftVersion = "26.3-snapshot-2"
     minecraftJarType = MinecraftJarType.SERVER
 
     val args = mutableListOf(
